d20aaf772c9569e1ffe2b4fa11a4f6cf52c3aa10
wei li <wei.li@weideMacBook-Pro.local>
2026-06-23 22:41:11 +0800

    feat(rich-text): add layout animation to BubbleMenu via Framer Motion

--- components/rich-text/menus/text-menu/text-menu.tsx
+++ components/rich-text/menus/text-menu/text-menu.tsx
@@ -1,4 +1,5 @@
 import { RichTextEditor, useRichTextEditor } from "@heroui-pro/react";
+import { motion } from "motion/react";
 import { Icon } from "@iconify/react";
 import { LinkMenuEdit } from "../link-menu/link-menu-edit";
 import {
@@ -24,7 +25,6 @@ import { TextColorPicker } from "./components/text-color-picker";
 import { BgColorPicker } from "./components/bg-color-picker";
 
 export function TextMenu() {
-  const { editor } = useRichTextEditor();
   const states = useTextMenuStates();
   const commands = useRichTextCommands();
 
@@ -40,93 +40,99 @@ export function TextMenu() {
         (document.querySelector("[data-slot='modal-dialog']") as HTMLElement) || document.body
       }
     >
-      <RichTextEditor.ToolbarGroup aria-label="">
-        <FontSizePicker value={states.fontSize} onChange={commands.setFontSize} />
-        <FontFamilyPicker value={states.fontFamily} onChange={commands.setFontFamily} />
-        <LineHeightPicker value={states.lineHeight} onChange={commands.setLineHeight} />
-      </RichTextEditor.ToolbarGroup>
+      <motion.div
+        layout
+        transition={{ type: "spring", stiffness: 300, damping: 30 }}
+        className="flex items-center"
+      >
+        <RichTextEditor.ToolbarGroup aria-label="">
+          <FontSizePicker value={states.fontSize} onChange={commands.setFontSize} />
+          <FontFamilyPicker value={states.fontFamily} onChange={commands.setFontFamily} />
+          <LineHeightPicker value={states.lineHeight} onChange={commands.setLineHeight} />
+        </RichTextEditor.ToolbarGroup>
 
-      <RichTextEditor.ToolbarSeparator />
+        <RichTextEditor.ToolbarSeparator />
 
-      <RichTextEditor.ToolbarGroup aria-label="Text Formatting">
-        <RichTextEditor.ToggleButton command="bold" tooltip="Bold">
-          <Icon icon="gravity-ui:bold" />
-        </RichTextEditor.ToggleButton>
-        <RichTextEditor.ToggleButton command="italic" tooltip="Italic">
-          <Icon icon="gravity-ui:italic" />
-        </RichTextEditor.ToggleButton>
-        <RichTextEditor.ToggleButton command="underline" tooltip="Underline">
-          <Icon icon="gravity-ui:underline" />
-        </RichTextEditor.ToggleButton>
-        <RichTextEditor.ToggleButton command="strike" tooltip="Strike">
-          <Icon icon="gravity-ui:strikethrough" />
-        </RichTextEditor.ToggleButton>
-        <RichTextEditor.ToggleButton command="code" tooltip="Code">
-          <Icon icon="gravity-ui:code" />
-        </RichTextEditor.ToggleButton>
-      </RichTextEditor.ToolbarGroup>
+        <RichTextEditor.ToolbarGroup aria-label="Text Formatting">
+          <RichTextEditor.ToggleButton command="bold" tooltip="Bold">
+            <Icon icon="gravity-ui:bold" />
+          </RichTextEditor.ToggleButton>
+          <RichTextEditor.ToggleButton command="italic" tooltip="Italic">
+            <Icon icon="gravity-ui:italic" />
+          </RichTextEditor.ToggleButton>
+          <RichTextEditor.ToggleButton command="underline" tooltip="Underline">
+            <Icon icon="gravity-ui:underline" />
+          </RichTextEditor.ToggleButton>
+          <RichTextEditor.ToggleButton command="strike" tooltip="Strike">
+            <Icon icon="gravity-ui:strikethrough" />
+          </RichTextEditor.ToggleButton>
+          <RichTextEditor.ToggleButton command="code" tooltip="Code">
+            <Icon icon="gravity-ui:code" />
+          </RichTextEditor.ToggleButton>
+        </RichTextEditor.ToolbarGroup>
 
-      <RichTextEditor.ToolbarSeparator orientation="vertical" />
+        <RichTextEditor.ToolbarSeparator orientation="vertical" />
 
-      <TextColorPicker value={states.textColor} onChange={commands.setTextColor} />
-      <BgColorPicker value={states.backgroundColor} onChange={commands.setBackgroundColor} />
+        <TextColorPicker value={states.textColor} onChange={commands.setTextColor} />
+        <BgColorPicker value={states.backgroundColor} onChange={commands.setBackgroundColor} />
 
-      <RichTextEditor.LinkPopover isOpen={state.isOpen} onOpenChange={state.setOpen}>
-        <RichTextEditor.LinkPopover.Trigger isIconOnly>
-          <Icon icon="gravity-ui:link" />
-        </RichTextEditor.LinkPopover.Trigger>
-        <RichTextEditor.LinkPopover.Content>
-          <LinkMenuEdit onCancel={state.close} />
-        </RichTextEditor.LinkPopover.Content>
-      </RichTextEditor.LinkPopover>
+        <RichTextEditor.LinkPopover isOpen={state.isOpen} onOpenChange={state.setOpen}>
+          <RichTextEditor.LinkPopover.Trigger isIconOnly>
+            <Icon icon="gravity-ui:link" />
+          </RichTextEditor.LinkPopover.Trigger>
+          <RichTextEditor.LinkPopover.Content>
+            <LinkMenuEdit onCancel={state.close} />
+          </RichTextEditor.LinkPopover.Content>
+        </RichTextEditor.LinkPopover>
 
-      <RichTextEditor.ToolbarSeparator orientation="vertical" />
+        <RichTextEditor.ToolbarSeparator orientation="vertical" />
 
-      <Popover isOpen={moreOverlayState.isOpen} onOpenChange={moreOverlayState.setOpen}>
-        <Button isIconOnly size="sm" variant="ghost">
-          <Icon icon="gravity-ui:ellipsis-vertical" />
-        </Button>
-        <Popover.Content>
-          <Popover.Dialog className="rounded-2xl p-1">
-            <Popover.Arrow />
-            <RichTextEditor.ToolbarGroup>
-              <ToggleButtonToolbar command="subscript" onPress={moreOverlayState.close}>
-                <Icon icon="gravity-ui:superscript" />
-              </ToggleButtonToolbar>
+        <Popover isOpen={moreOverlayState.isOpen} onOpenChange={moreOverlayState.setOpen}>
+          <Button isIconOnly size="sm" variant="ghost">
+            <Icon icon="gravity-ui:ellipsis-vertical" />
+          </Button>
+          <Popover.Content>
+            <Popover.Dialog className="rounded-2xl p-1">
+              <Popover.Arrow />
+              <RichTextEditor.ToolbarGroup>
+                <ToggleButtonToolbar command="subscript" onPress={moreOverlayState.close}>
+                  <Icon icon="gravity-ui:superscript" />
+                </ToggleButtonToolbar>
 
-              <ToggleButtonToolbar command="superscript" onPress={moreOverlayState.close}>
-                <Icon icon="gravity-ui:superscript" />
-              </ToggleButtonToolbar>
+                <ToggleButtonToolbar command="superscript" onPress={moreOverlayState.close}>
+                  <Icon icon="gravity-ui:superscript" />
+                </ToggleButtonToolbar>
 
-              <RichTextEditor.ToolbarSeparator />
+                <RichTextEditor.ToolbarSeparator />
 
-              <ToggleButtonToolbar command="alignLeft" onPress={moreOverlayState.close}>
-                <Icon icon="gravity-ui:text-align-left" />
-              </ToggleButtonToolbar>
+                <ToggleButtonToolbar command="alignLeft" onPress={moreOverlayState.close}>
+                  <Icon icon="gravity-ui:text-align-left" />
+                </ToggleButtonToolbar>
 
-              <ToggleButtonToolbar command="alignCenter" onPress={moreOverlayState.close}>
-                <Icon icon="gravity-ui:text-align-center" />
-              </ToggleButtonToolbar>
+                <ToggleButtonToolbar command="alignCenter" onPress={moreOverlayState.close}>
+                  <Icon icon="gravity-ui:text-align-center" />
+                </ToggleButtonToolbar>
 
-              <ToggleButtonToolbar command="alignRight" onPress={moreOverlayState.close}>
-                <Icon icon="gravity-ui:text-align-right" />
-              </ToggleButtonToolbar>
-              <ToggleButtonToolbar command="alignJustify" onPress={moreOverlayState.close}>
-                <Icon icon="gravity-ui:text-align-justify" />
-              </ToggleButtonToolbar>
+                <ToggleButtonToolbar command="alignRight" onPress={moreOverlayState.close}>
+                  <Icon icon="gravity-ui:text-align-right" />
+                </ToggleButtonToolbar>
+                <ToggleButtonToolbar command="alignJustify" onPress={moreOverlayState.close}>
+                  <Icon icon="gravity-ui:text-align-justify" />
+                </ToggleButtonToolbar>
 
-              <RichTextEditor.ToolbarSeparator />
+                <RichTextEditor.ToolbarSeparator />
 
-              <RichTextEditor.CommandButton onCommand={() => {}}>
-                <Icon icon="gravity-ui:text-outdent" />
-              </RichTextEditor.CommandButton>
-              <RichTextEditor.CommandButton onCommand={() => {}}>
-                <Icon icon="gravity-ui:text-indent" />
-              </RichTextEditor.CommandButton>
-            </RichTextEditor.ToolbarGroup>
-          </Popover.Dialog>
-        </Popover.Content>
-      </Popover>
+                <RichTextEditor.CommandButton onCommand={() => {}}>
+                  <Icon icon="gravity-ui:text-outdent" />
+                </RichTextEditor.CommandButton>
+                <RichTextEditor.CommandButton onCommand={() => {}}>
+                  <Icon icon="gravity-ui:text-indent" />
+                </RichTextEditor.CommandButton>
+              </RichTextEditor.ToolbarGroup>
+            </Popover.Dialog>
+          </Popover.Content>
+        </Popover>
+      </motion.div>
     </RichTextEditor.BubbleMenu>
   );
 }
