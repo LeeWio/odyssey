edf95d52e442e6d01a8e0607695dd3d0b213aa40
wei.li <just.vireo@gmail.com>
2026-07-24 21:37:11 +0800

    style(clean): format files with prettier

--- app/oauth2/redirect/page.tsx
+++ app/oauth2/redirect/page.tsx
@@ -20,7 +20,7 @@ const decodeJwt = (token: string) => {
       window
         .atob(base64)
         .split("")
-        .map((c) => `%${(`00${c.charCodeAt(0).toString(16)}`).slice(-2)}`)
+        .map((c) => `%${`00${c.charCodeAt(0).toString(16)}`.slice(-2)}`)
         .join("")
     );
     return JSON.parse(jsonPayload);
