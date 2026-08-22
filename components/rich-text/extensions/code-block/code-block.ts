d311f1dc92b2f780bc2dd3e24e76293520a30c69
wei.li <just.vireo@gmail.com>
2026-08-23 03:11:14 +0800

    feat(rich-text): set higher priority on custom CodeBlockLowlight extension to override default StarterKit CodeBlock

--- components/rich-text/extensions/code-block/code-block.ts
+++ components/rich-text/extensions/code-block/code-block.ts
@@ -6,6 +6,11 @@ import { createLowlight, common } from "lowlight";
 const lowlight = createLowlight(common);
 
 export const CodeBlockLowlight = TiptapCodeBlockLowlight.extend({
+  // Set priority higher than the default CodeBlock (which has default priority 100)
+  // to ensure Tiptap resolves the 'codeBlock' schema, input rules, and keymaps
+  // to our custom CodeBlockLowlight node instead of the default StarterKit CodeBlock.
+  priority: 1000,
+
   addNodeView() {
     return ReactNodeViewRenderer(CodeBlockNodeView);
   },
